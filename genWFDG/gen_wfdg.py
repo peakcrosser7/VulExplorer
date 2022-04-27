1f9656d5b66a8b9ac0498c3b9df453dd426811fd
Unravel <1621690242@qq.com>
2022-04-27 20:23:36 +0800

    update gen_wfdg.py

--- genWFDG/gen_wfdg.py
+++ genWFDG/gen_wfdg.py
@@ -3,13 +3,13 @@ import sys
 import time
 from typing import List
 
-import wfdg_generator
+from genWFDG import wfdg_generator
 
 DEBUG = True
 
-FILE_TYPES = {'.c', '.c#vul', '.c#fixed'}
+FILE_TYPE = '.c'
 
-g_header_dirs = ['-I/usr/local/lib/clang/9.0.0/include']
+g_header_dirs = ['-w', '-I/usr/local/lib/clang/9.0.0/include']
 
 g_total_wfdgs = 0
 
@@ -17,7 +17,7 @@ g_total_wfdgs = 0
 def correct_file_types(filename: str) -> bool:
     """判断文件类型是否为c/cpp"""
     ret = os.path.splitext(filename)
-    return len(ret) != 0 and ret[1] in FILE_TYPES
+    return len(ret) != 0 and ret[1] == FILE_TYPE
 
 
 def get_header_dirs(header_dir: str) -> List[str]:
@@ -28,34 +28,36 @@ def get_header_dirs(header_dir: str) -> List[str]:
     return header_dirs
 
 
-def get_local_header_dirs(dirpath: str) -> list:
+def get_local_header_dirs(dirpath: str) -> List[str]:
+    head_list = get_header_dirs(dirpath)
     header_dir = os.path.join(dirpath, 'include')
-    if not os.path.exists(header_dir):
-        return []
-    return get_header_dirs(header_dir)
+    if os.path.exists(header_dir):
+        head_list.extend(get_header_dirs(header_dir))
+    return head_list
 
 
-def gen_WFDGs_by_generator(filepath: str, output_dir: str, header_list: List[str], dest_func: str = "",
-                           sensitive_line: int = 0):
+def gen_WFDGs_by_generator(filepath: str, header_list: List[str], dest_func: str = "", not_use_sensitive=False,
+                           sensitive_line: int = 0) -> list:
     if DEBUG:
         print('Generate WFDGs from <%s>' % filepath)
 
     config = wfdg_generator.Configuration()
-    config.specify_func(dest_func, sensitive_line)
+    config.specify_func(dest_func, sensitive_line, not_use_sensitive)
     wfdgs = wfdg_generator.gen_WFDGs([filepath], config, header_list)
     num = len(wfdgs)
     global g_total_wfdgs
     g_total_wfdgs += num
     print(num)
+    return wfdgs
 
 
-def gen_all_WFDGs(input_dir: str, output_dir: str, header_dir: str):
+def gen_all_WFDGs(input_dir: str, header_dir: str, output_dir: str):
     if not os.path.exists(input_dir) or not os.path.exists(header_dir):
         return
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    header_list = ['-w']
+    header_list = []
     header_list.extend(g_header_dirs)
     header_list.extend(get_header_dirs(header_dir))
 
@@ -64,7 +66,7 @@ def gen_all_WFDGs(input_dir: str, output_dir: str, header_dir: str):
             filename = input_dir.split('/')[-1].strip()
             dirpath = input_dir.rstrip('/' + filename)
             header_list.extend(get_local_header_dirs(dirpath))
-            gen_WFDGs_by_generator(input_dir, output_dir, header_list)
+            gen_WFDGs_by_generator(input_dir, header_list)
         return
 
     for dirpath, _, filenames in os.walk(input_dir):
@@ -73,7 +75,7 @@ def gen_all_WFDGs(input_dir: str, output_dir: str, header_dir: str):
         tmp_header_list.extend(get_local_header_dirs(dirpath))
         for filename in filenames:
             if correct_file_types(filename):
-                gen_WFDGs_by_generator(os.path.join(dirpath, filename), output_dir, tmp_header_list)
+                gen_WFDGs_by_generator(os.path.join(dirpath, filename), tmp_header_list)
 
 
 if __name__ == '__main__':
@@ -87,7 +89,7 @@ if __name__ == '__main__':
     s = time.time()
     print("start time: ", s)
 
-    gen_all_WFDGs(g_input_dir, g_output_dir, g_header_dir)
+    gen_all_WFDGs(g_input_dir, g_header_dir, g_output_dir)
 
     s = time.time() - s
     m, s = divmod(s, 60)
