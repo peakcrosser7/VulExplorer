c0cc4d57412f07375261101266ba10d7a5ab57a3
Unravel <1621690242@qq.com>
2022-05-17 15:14:31 +0800

    update detect.py

--- cmpWFDG/detect.py
+++ cmpWFDG/detect.py
@@ -1,15 +1,19 @@
 import os.path
-import sys
+import time
 from typing import List, Set
 
 import global_config
 from cmpWFDG import cmp_wfdg
 from genWFDG import gen_wfdg
+from utils import my_time
 from utils.log import *
 
 DEBUG = True
 
 g_header_dirs = ['-w', '-I/usr/local/lib/clang/9.0.0/include']
+
+g_cost_time = 0
+g_wfdgs_cnt = 0
 
 
 def correct_file_types(filename: str) -> bool:
@@ -45,25 +49,37 @@ def get_header_dirs(dir_path: str, head_path: str) -> List[str]:
 
 
 def cmp_with_dataset(file_path: str, wfdgs: list, dataset: list):
+    global g_cost_time, g_wfdgs_cnt
     vul_result = []
     pinfo('compare <%s> with vul dataset...' % file_path)
-    for wfdg in wfdgs:
-        for vul_info in dataset:
+    for vul_info in dataset:
+        checked_funcs = set()
+        for wfdg in wfdgs:
+            if wfdg.get_func_name() in checked_funcs:
+                continue
+            s = time.time()
             sim = cmp_wfdg.compare_wfdg(wfdg, vul_info['vul_wfdg'])
-            if sim > global_config.VUL_THRESHOLD \
-                    and sim > cmp_wfdg.compare_wfdg(wfdg, vul_info['fixed_wfdg']):
-                vul = {
-                    'CVE_id': vul_info['CVE_id'],
-                    'file_path': file_path,
-                    'func_name': wfdg.get_func_name()
-                }
-                vul_result.append(vul)
-                pinfo('find vul(CVE_id: %s) in path:%s func:%s' %
-                      (vul['CVE_id'], file_path, vul['func_name']))
+            g_cost_time += time.time() - s
+            g_wfdgs_cnt += 1
+            if sim > global_config.VUL_THRESHOLD:
+                s = time.time()
+                p_sim = cmp_wfdg.compare_wfdg(wfdg, vul_info['fixed_wfdg'])
+                g_cost_time += time.time() - s
+                g_wfdgs_cnt += 1
+                if sim > p_sim:
+                    vul = {
+                        'CVE_id': vul_info['CVE_id'],
+                        'file_path': file_path,
+                        'func_name': wfdg.get_func_name()
+                    }
+                    checked_funcs.add(wfdg.get_func_name())
+                    vul_result.append(vul)
+                    pinfo('find vul(CVE_id: %s) in path:%s func:%s' %
+                          (vul['CVE_id'], file_path, vul['func_name']))
     return vul_result
 
 
-def detect_by_cmp(input_path: str, head_path: str, dataset: list, keywords: Set[str]):
+def detect_by_cmp(input_path: str, head_path: str, dataset: list, keywords: Set[str] = None):
     if not os.path.exists(input_path):
         perr('input path:%s does not exist' % input_path)
         return None
@@ -88,6 +104,8 @@ def detect_by_cmp(input_path: str, head_path: str, dataset: list, keywords: Set[
         tmp_header_list = []
         tmp_header_list.extend(header_list)
         tmp_header_list.extend(get_header_dirs(dirpath, head_path))
+        # for h in tmp_header_list:
+        #     print('--extra-arg="%s" ' % h, end='')
         for filename in filenames:
             if correct_file_types(filename):
                 file_path = os.path.join(dirpath, filename)
@@ -97,10 +115,21 @@ def detect_by_cmp(input_path: str, head_path: str, dataset: list, keywords: Set[
     return vul_result
 
 
+def get_cmp_time() -> float:
+    if g_wfdgs_cnt == 0:
+        return -1.
+    return float(g_cost_time) / g_wfdgs_cnt
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("python3 extract_func.py <input_dir> <output_folder> <header_dir>\n")
-        exit(-1)
-    g_input_dir = sys.argv[1]
-    g_output_dir = sys.argv[2]
-    g_header_dir = sys.argv[3]
+    g_input_dir = global_config.DETECT_PATH
+    g_header_dir = global_config.HEAD_PATH
+    start_time = time.time()
+    pinfo('start vulnerability detection at %s, detection path: %s, head path: %s'
+          % (my_time.get_time_str(start_time), global_config.DETECT_PATH, global_config.HEAD_PATH))
+    detect_by_cmp(g_input_dir, g_header_dir, [])
+    end_time = time.time()
+    pinfo('end vulnerability detection at %s' % my_time.get_time_str(end_time))
+    print('start time: %s    end time: %s    cost time: %s' %
+          (my_time.get_time_str(start_time), my_time.get_time_str(end_time),
+           my_time.get_time_interval(end_time - start_time)))
